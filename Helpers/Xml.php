ac93888f5ccdac1d68651bb06e11447edd6dea86
uavster <ignacio.mellado@gmail.com>
2013-07-12 01:00:41 +0200

    htmlTextSummary removes <table> tags too

--- Helpers/Xml.php
+++ Helpers/Xml.php
@@ -309,6 +309,7 @@ class Helpers_Xml {
 		$html = self::removeTags($html, 'select', 0, true);
 		$html = self::removeTags($html, 'textarea', 0, true);
 		$html = self::removeTags($html, 'button', 0, true);
+		$html = self::removeTags($html, 'table', 0, true);
 
 		$paragraphs = self::findTagsInfo($html, array('name' => 'p', 'attrName' => null, 'attrValue' => null));
 		$summary = '';
